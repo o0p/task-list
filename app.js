05a4c927ecc719281354bde5010efe8c644ffec4
Russell <russelldev@protonmail.com>
2020-12-17 20:10:23 +0000

    Add app.js - addTask func complete

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ function addTask(e) {
     if(taskInput.value === '') {
         alert('add a task');
     }
+    //create li element
+    const li = document.createElement('li');
+    //add class
+    li.className = 'collection-item';
+    //create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link element
+    const link = document.createElement('a');
+    //add class
+    link.className = 'delete-item secondary-content';
+    //add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append the link to li
+    li.appendChild(link);
+    //append li to ul
+    taskList.appendChild(li);
 
+    
     e.preventDefault();
-}
+} 
