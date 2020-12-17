a3b0c3b96821c1bf61ce4d6092b5edaccd1703d8
Russell <russelldev@protonmail.com>
2020-12-17 19:56:15 +0000

    Add app.js - addTask func

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#tasks');
+const taskInput = document.querySelector('#task');
+loadEventListeners();
+
+function loadEventListeners() {
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('add a task');
+    }
+
+    e.preventDefault();
+}
