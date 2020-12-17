bb4e08f7067ed0338b4387842c1f579a70ec7663
Russell <russelldev@protonmail.com>
2020-12-17 21:57:06 +0000

    Add app.js - save to local storage works

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners() {
+    //DOM load event
+    document.addEventListener('DOMContentLoaded', getTasks);
     //add task event
     form.addEventListener('submit', addTask);
     //remove task event
@@ -16,6 +18,38 @@ function loadEventListeners() {
     filter.addEventListener('keyup', filterTasks);
 }
 
+//get tasks from ls
+function getTasks(){
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task){
+            //create li element
+    const li = document.createElement('li');
+    //add class
+    li.className = 'collection-item';
+    //create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    //create new link element
+    const link = document.createElement('a');
+    //add class
+    link.className = 'delete-item secondary-content';
+    //add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append the link to li
+    li.appendChild(link);
+
+    //append li to ul
+    taskList.appendChild(li);
+
+    });
+}
+
+//add task
 function addTask(e) {
     if(taskInput.value === '') {
         alert('add a task');
