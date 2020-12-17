32da6ec1a45088ee013a733dc8f4d8c4eb8edcef
Russell <russelldev@protonmail.com>
2020-12-17 22:07:07 +0000

    Add app.js - remove task from LS

--- app.js
+++ app.js
@@ -110,7 +110,19 @@ function removeTask(e){
 
 //remove from LS
 function removeTaskFromLocalStorage(taskItem){
-    console.log(taskItem);
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 
